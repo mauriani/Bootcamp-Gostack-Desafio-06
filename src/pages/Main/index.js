6d16a8d7addd200e73b7fbaa019a42a259a1180c
mauriani <maurianim@gmail.com>
2020-12-21 16:31:43 -0300

    inicio a api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import {Container, Form, Input, SubmitButton} from './styles';
 
-const Main = () => {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar Usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" color="#fff" size={20} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
-};
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      newUser: '',
+    };
+  }
 
-export default Main;
+  handleAddUser = async () => {
+    const {users, newUser} = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+    // faz o teclado sumir
+    Keyboard.dismiss();
+  };
+  render() {
+    const {users} = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            onChangeText={(text) => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" color="#fff" size={20} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
